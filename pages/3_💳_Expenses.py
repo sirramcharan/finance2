0712e483193e0fd340685ba8553661fa8b38193d
sirramcharan <sirramcharan.tejareddy@mba.christuniversity.in>
2026-06-05 09:17:14 +0530

    Update 3_💳_Expenses.py

--- pages/3_💳_Expenses.py
+++ pages/3_💳_Expenses.py
@@ -10,7 +10,9 @@ st.set_page_config(
     layout="wide",
     initial_sidebar_state="collapsed",
 )
+from utils.navigation import render_top_nav
 
+render_top_nav()
 from utils.theme import inject_css, alert_card, progress_bar
 from utils.data_manager import DataManager
 from utils.formatters import (
